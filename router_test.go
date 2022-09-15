826605fa199ddeaba3e7df67baa3dcc4f3e4626c
Emilis <emilis.mitrikas@gmail.com>
2022-09-15 19:49:33 +0100

    Doing some performance testing for the router

---
+++ router_test.go
@@ -0,0 +1,37 @@
+package router
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"testing"
+)
+
+var stringToSplit = "/one/two/three/four/"
+var stringSeparator = "/"
+var bytesToSplit = []byte("/one/two/three/four")
+var bytesSeparator = []byte("/")
+
+func BenchmarkEntry_SplitStrings(b *testing.B) {
+	tidyString := stringToSplit
+
+	if tidyString[0] == 47 {
+		tidyString = tidyString[1:]
+	}
+
+	if tidyString[len(tidyString)-1] == 47 {
+		tidyString = tidyString[:len(tidyString)-1]
+	}
+
+	fmt.Println(tidyString)
+
+	for n := 0; n < b.N; n++ {
+		strings.Split(tidyString, stringSeparator)
+	}
+}
+
+func BenchmarkEntry_SplitBytes(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		bytes.Split(bytesToSplit, bytesSeparator)
+	}
+}
