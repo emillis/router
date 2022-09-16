31e19df7ed074fde275a7694c933c34ee5065364
Emilis <emilis.mitrikas@gmail.com>
2022-09-16 18:41:57 +0100

    Done some more testing layouts of the module

--- router_test.go
+++ router_test.go
@@ -12,6 +12,26 @@ var stringSeparator = "/"
 var bytesToSplit = []byte("/one/two/three/four")
 var bytesSeparator = []byte("/")
 
+type Route struct {
+	partitionedPath []string
+}
+
+func New(path string) *Route {
+	tidyString := path
+
+	if tidyString[0] == 47 {
+		tidyString = tidyString[1:]
+	}
+
+	if tidyString[len(tidyString)-1] == 47 {
+		tidyString = tidyString[:len(tidyString)-1]
+	}
+
+	return &Route{
+		partitionedPath: strings.Split(tidyString, "/"),
+	}
+}
+
 func BenchmarkEntry_SplitStrings(b *testing.B) {
 	tidyString := stringToSplit
 
